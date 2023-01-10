012febc377e3e10a022743edd9979e2e5b01a463
Annasaheb Kute <anna@cloudaeon.net>
2023-01-10 12:24:31 +0530

    UI fixd

--- js/custom.js
+++ js/custom.js
@@ -15,20 +15,95 @@ const onScroll = () => {
 window.addEventListener('scroll', onScroll)
 
 // slider
-const slidesContainer = document.getElementById("slides-container");
+// const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+const prevButton = document.querySelectorAll(".slide-arrow-prev");
+const nextButton = document.querySelectorAll(".slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
+
+function nextBtn(){
+    const slideWidth = slide.clientWidth;
+    nextButton[0].nextElementSibling.scrollLeft += slideWidth;
+    var nextButtonnar = nextButton.nextElementSibling
+    console.log(nextButton[0].nextElementSibling)
+};
+
+function prevBtn(){   
+    const slideWidth = slide.clientWidth;
+    prevButton[0].previousElementSibling.scrollLeft -= slideWidth;
+    console.log(prevButton[0].previousElementSibling)
+};
+
+
+
+// slider TESTIMONIALS
+const slidesContainerts = document.getElementById("slides-containerts");
+const slidets = document.querySelector(".slide");
+const prevButtonts = document.getElementById("slide-arrow-prevts");
+const nextButtonts = document.getElementById("slide-arrow-nextts");
+
+nextButtonts.addEventListener("click", () => {
+  const slideWidthts = slide.clientWidth;
+  slidesContainerts.scrollLeft += slideWidthts;
 });
 
-prevButton.addEventListener("click", () => {    
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+prevButtonts.addEventListener("click", () => {    
+  const slideWidthts = slidets.clientWidth;
+  slidesContainerts.scrollLeft -= slideWidthts;
 });
+
+// Slider auto
+Slider();
+function Slider() {
+  const carouselSlides = document.querySelectorAll('.slideFull');
+  const btnPrev = document.querySelector('.prev');
+  const btnNext = document.querySelector('.next');
+  const dotsSlide = document.querySelector('.dots-container');
+  let currentSlide = 0;
+  const activeDot = function (slideAut) {
+      document.querySelectorAll('.dot').forEach(dot => dot.classList.remove('active'));
+      document.querySelector(`.dot[data-slide="${slideAut}"]`).classList.add('active');
+  };
+  activeDot(currentSlide);
+
+  const changeSlide = function (slides) {
+      carouselSlides.forEach((slideAut, index) => (slideAut.style.transform = `translateX(${100 * (index - slides)}%)`));
+  };
+  changeSlide(currentSlide);
+
+//   btnNext.addEventListener('click', function () {
+//       currentSlide++; 
+//       if (carouselSlides.length - 1 < currentSlide) {
+//           currentSlide = 0;
+//       };
+//       changeSlide(currentSlide);
+//       activeDot(currentSlide);
+// });
+  // btnPrev.addEventListener('click', function () {
+  //     currentSlide--;
+  //     if (0 >= currentSlide) {
+  //         currentSlide = 0;
+  //     }; 
+  //     changeSlide(currentSlide);
+  //     activeDot(currentSlide);
+  // });
+  dotsSlide.addEventListener('click', function (e) {
+      if (e.target.classList.contains('dot')) {
+          const slideAut = e.target.dataset.slideAut;
+          changeSlide(slideAut);
+          activeDot(slideAut);
+      }
+  });
+  setInterval(function () {
+   currentSlide++; 
+      if (carouselSlides.length - 1 < currentSlide) {
+          currentSlide = 0;
+      };
+      changeSlide(currentSlide);
+      activeDot(currentSlide);
+  }, 4000);
+};
+
 
 //Login page
 function closeBt() {
@@ -96,29 +171,70 @@ function signUPTab() {
 
 }
 
-// SIGNUP
-
+// SIGNUP redio btn
 phoneCheckT();
 emailCheckT();
-
 function phoneCheckT() {
   if (document.getElementById('phoneCheck').checked) {
       document.getElementById('phoneCheckfield').style.display = 'block';
       document.getElementById('emailCheckfield').style.display = 'none';
 
-      document.getElementById('phoneCheckfieldin').style.display = 'block';
-      document.getElementById('emailCheckfieldin').style.display = 'none';
   }
 }
-
 function emailCheckT() {
   if (document.getElementById('emailCheck').checked) {
       document.getElementById('emailCheckfield').style.display = 'block';
       document.getElementById('phoneCheckfield').style.display = 'none';
-
+  }
+}
+function phoneCheckTin() {
+  if (document.getElementById('phoneCheckin').checked) {
+      document.getElementById('phoneCheckfieldin').style.display = 'block';
+      document.getElementById('emailCheckfieldin').style.display = 'none';
+  }
+}
+function emailCheckTin() {
+  if (document.getElementById('emailCheckin').checked) {
       document.getElementById('phoneCheckfieldin').style.display = 'none';
       document.getElementById('emailCheckfieldin').style.display = 'block';
   }
+}
+
+// tab
+
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+document.getElementById('tabs').onclick= function (event) {
+    var target=event.target;
+    if (target.className=='tab') {
+       for (var i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+           }
+       }
+    }
+}
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('whiteborder');
+    }
+}
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
 }
 
 
