9dac664dd15eb7a42ff5b7f8af9a1d1aa34efffb
Annasaheb Kute <anna@cloudaeon.net>
2023-01-15 21:31:32 +0530

    UI fixed

--- js/courseDetailsAndProfile.js
+++ js/courseDetailsAndProfile.js
@@ -121,3 +121,114 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+
+//Login page
+function closeBt() {
+  var closebtn = document.getElementById("_loginPupPup");
+  var loginPupPupBg = document.getElementById("_loginPupPupBg");
+  if (closebtn.style.display === "block") {
+    closebtn.style.display = "none";
+    loginPupPupBg.style.display = "none";
+  } else {
+    closebtn.style.display = "block";
+    loginPupPupBg.style.display = "block";
+  }
+}
+// SIGNUP
+function closeBt() {
+  var closebtn = document.getElementById("_loginPupPup");
+  var loginPupPupBg = document.getElementById("_loginPupPupBg");
+  if (closebtn.style.display === "block") {
+    closebtn.style.display = "none";
+    loginPupPupBg.style.display = "none";
+  } else {
+    closebtn.style.display = "block";
+    loginPupPupBg.style.display = "block";
+  }
+}
+// SIGNIN
+var signup = document.getElementById("_signup");
+var signin = document.getElementById("_signin");
+var btnSignup = document.getElementById("_btnSignup");
+var btnSignin = document.getElementById("_btnSignin");
+
+function signinTab() {
+  if (signup.style.display === "none") {
+    signin.style.display = "block";
+    signup.style.display = "none";
+    btnSignup.classList.remove('ab-active');
+    btnSignin.classList.add('ab-active');
+    // this.target.classList.remove('ab-active');
+  } 
+  if (signup.style.display === "block") {
+    signin.style.display = "block";
+    signup.style.display = "none";
+    btnSignup.classList.remove('ab-active');
+    btnSignin.classList.add('ab-active');
+    // this.target.classList.remove('ab-active');
+  } 
+
+    
+
+}
+// SIGNUP
+function signUPTab() {
+  if (signup.style.display === "none") {
+    signup.style.display = "block";
+    signin.style.display = "none";
+    btnSignup.classList.add('ab-active');
+    btnSignin.classList.remove('ab-active');
+  }
+  if (signup.style.display === "block") {
+    signup.style.display = "block";
+    signin.style.display = "none";
+    btnSignup.classList.add('ab-active');
+    btnSignin.classList.remove('ab-active');
+  }
+
+}
+
+// SIGNUP redio btn
+phoneCheckT();
+emailCheckT();
+phoneCheckTin();
+emailCheckTin();
+function phoneCheckT() {
+  if (document.getElementById('phoneCheck').checked) {
+      document.getElementById('phoneCheckfield').style.display = 'block';
+      document.getElementById('emailCheckfield').style.display = 'none';
+
+  }
+}
+function emailCheckT() {
+  if (document.getElementById('emailCheck').checked) {
+      document.getElementById('emailCheckfield').style.display = 'block';
+      document.getElementById('phoneCheckfield').style.display = 'none';
+  }
+}
+function phoneCheckTin() {
+  if (document.getElementById('phoneCheckin').checked) {
+      document.getElementById('phoneCheckfieldin').style.display = 'block';
+      document.getElementById('emailCheckfieldin').style.display = 'none';
+  }
+}
+function emailCheckTin() {
+  if (document.getElementById('emailCheckin').checked) {
+      document.getElementById('phoneCheckfieldin').style.display = 'none';
+      document.getElementById('emailCheckfieldin').style.display = 'block';
+  }
+}
+
+
+//vedio page
+function vediobt() {
+  var closebtn = document.getElementById("_videoPupPup");
+  var loginPupPupBg = document.getElementById("_videoPupPupBg");
+  if (closebtn.style.display === "block") {
+    closebtn.style.display = "none";
+    loginPupPupBg.style.display = "none";
+  } else {
+    closebtn.style.display = "block";
+    loginPupPupBg.style.display = "block";
+  }
+}
